47e7df81ffb58d06688c9d2e09026ccf91f94385
Rosemary <thebluesign@gmail.com>
2021-07-25 22:11:22 -0700

    routine button border

--- src/pages/WorkoutRoutine/index.js
+++ src/pages/WorkoutRoutine/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import { makeStyles } from '@material-ui/core/styles';
+import { lighten } from '@material-ui/core';
 import { routines } from 'data';
 import RoutineTable from 'components/Routine';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   group: {
     width: '100%',
   },
   btn: {
     width: '50%',
   },
+  active: {
+    backgroundColor: 'white !important',
+    border: `1px solid ${lighten(theme.palette.primary.main, .5)} !important`,
+    color: `${theme.palette.primary.main} !important`
+  }
 }));
 
 function WorkoutRoutine() {
@@ -19,20 +25,21 @@ function WorkoutRoutine() {
   const [routineName, setRoutine] = React.useState('hamptons');
   const [workoutIdx, setWorkout] = React.useState(0);
   const routine = routines[routineName];
+  const btnClass = (value) => `${classes.btn} ${routineName === value ? classes.active : ''}`
 
   return (
     <>
       <ToggleButtonGroup
+        className={classes.group}
         value={routineName}
-        indicatorColor="primary"
         exclusive={true}
         onChange={(_, value) => value && setRoutine(value)}
         aria-label="Hybrid Calisthenics Routines"
       >
-        <ToggleButton className={classes.btn} value="hamptons" aria-label="Hampton's Routine">
+        <ToggleButton className={btnClass('hamptons')} value="hamptons" aria-label="Hampton's Routine">
           Hampton's Routine
         </ToggleButton>
-        <ToggleButton className={classes.btn} value="workweek" aria-label="Work Week Routine">
+        <ToggleButton className={btnClass('workweek')} value="workweek" aria-label="Work Week Routine">
           Work Week Routine
         </ToggleButton>
       </ToggleButtonGroup>
